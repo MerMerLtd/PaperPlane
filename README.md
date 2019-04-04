1a1681b76f53785ffcd190c243405a6fb5d91ae2
Luphia Chang <luphia@mermer.cc>
2019-04-04 11:54:10 +0800

    Initial Project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PaperPlane
 Large File Transfer Service
+
+## Deploy
+```shell
+git clone https://github.com/MerMerLtd/PaperPlane
+cd PaperPlane
+npm i
+```
---
+++ src/backend/libs/LFS.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+const Bot = require(path.resolve(__dirname, 'Bot.js'));
+const Utils = require(path.resolve(__dirname, 'Utils.js'));
+
+class LFS extends Bot {
+  
+}
+
+module.exports = LFS;
