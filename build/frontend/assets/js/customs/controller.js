17a5e567f50cc685f2f665a9a8180ddb42003d82
HazelLiang <crocodilestear@gmail.com>
2019-04-05 22:05:24 +0800

    整理UX/UI有關的function

--- build/frontend/assets/js/customs/controller.js
+++ build/frontend/assets/js/customs/controller.js
@@ -1,6 +1,10 @@
 // =============================================================
 // Views
 const elements = {
+    alertSuccess: document.querySelector(".alert"),
+    addIcon: document.querySelector(".add__icon"),
+    addText: document.querySelector(".add__text"),
+    dropndDrop: document.querySelector(".box__dragndrop"),
     body: document.querySelector("body"),
     hint: document.querySelector(".card-header > .hint"),
     cardHeader: document.querySelector(".card-header"),
@@ -29,24 +33,26 @@ const renderFile = file =>{
 const renderFiles = files => {
     files.forEach(file => renderFile(file));
 }
-// over file-container 的提示
-const hintAction = () => {
-    const markup = `
-        <div class="alert alert-success" role="alert">
-            You're on the right track! Just put it down.
-        </div>
-    `;
-    elements.page.insertAdjacentHTML("afterbegin", markup);
-}
-// 放進 file-container 的 提示
-const hintReward = () => {
-    const markup = `
-        <div class="alert alert-success" role="alert">
-            Well done!
-        </div>
-    `;
-    elements.page.insertAdjacentHTML("afterbegin", markup);
-}
+
+// // over file-container 的提示
+// const hintAction = () => {
+//     const markup = `
+//         <div class="alert alert-success" role="alert">
+//             You're on the right track! Just put it down.
+//         </div>
+//     `;
+//     elements.page.insertAdjacentHTML("afterbegin", markup);
+// }
+// // 放進 file-container 的 提示
+// const hintReward = () => {
+//     const markup = `
+//         <div class="alert alert-success" role="alert">
+//             Well done!
+//         </div>
+//     `;
+//     elements.page.insertAdjacentHTML("afterbegin", markup);
+// }
+
 // 放進 “哪裡” 的 提示
 const hintLocation = () => {
    elements.cardHeader.classList.add("hint");
@@ -70,10 +76,66 @@ const File = {
 
 const state = {};
 
-const handleDragOver = evt => {
+const addMultiListener = (element, events, func) => {
+    events.split(" ").forEach(event => element.addEventListener(event, func, false));
+}
+const removeMultiListener = (element, events, func) => {
+    events.split(" ").forEach(event => element.removeEventListener(event, func, false));
+}
+
+const handleDefault = evt => {
     evt.stopPropagation();
     evt.preventDefault();
     evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+  
+}
+
+// drag file over the fileList 之後
+const handleInFileList = () => {
+
+    removeHintLocation();
+    elements.addIcon.classList.add("add__icon--small");
+    elements.addText.classList.add("add__text--small");
+    elements.fileList.classList.add("u-margin-top--sm");// 位置一時半會還沒調好
+    elements.alertSuccess.style.setProperty("--opacity", 1);
+
+}
+
+const handleOutFileList = () => {
+
+    hintLocation();
+    elements.addIcon.classList.remove("add__icon--small");
+    elements.addText.classList.remove("add__text--small");
+    elements.fileList.classList.remove("u-margin-top--sm");// 位置一時半會還沒調好
+    elements.alertSuccess.style.setProperty("--opacity", 0);
+
+
+}
+// drag file in the pageHeader
+const handleDragInPageHeader = evt => {
+
+    if(!isCurrentIn){
+        isCurrentIn = true;
+        hintLocation();
+    }
+    if(evt.target.matches(".box__dropzone, .box__dropzone * " || "fileList.length")){
+        handleInFileList();
+    }else{
+        handleOutFileList();
+    }
+}
+
+
+const handleDragoutPageHeader = evt => {
+
+    if(isCurrentIn){
+        isCurrentIn = false;
+        removeHintLocation();
+    }
+}
+
+const handleDrop = evt => {
+    elements.alertSuccess.style.setProperty("--opacity", 0);
 }
 
 // Check for the various File API support.
@@ -90,29 +152,18 @@ var isAdvancedUpload = function() {
 }();
 
 if(isAdvancedUpload){
+
     elements.dropZone.classList.add("has-advanced-upload");
+    addMultiListener(elements.pageHeader, "drag dragstart dragend dragover dragenter dragleave drop", evt => handleDefault(evt));
+    addMultiListener(elements.pageHeader, "dragover dragenter", evt => handleDragInPageHeader(evt));
+    addMultiListener(elements.pageHeader, "dragleave dragend drop",  evt => handleDragoutPageHeader(evt));
+    addMultiListener(elements.pageHeader, "drop",  evt => handleDragoutPageHeader(evt));
+
 }
 
 let isCurrentIn = false;
 
-elements.pageHeader.addEventListener("dragover", evt => {
-    console.log("hi");
-    // handleDragOver(evt);
-    
-    if(!isCurrentIn){
-        isCurrentIn = true;
-        hintLocation();
-    }
-});
-elements.pageHeader.addEventListener("dragleave", evt => {
-    console.log("bye");
-    handleDragOver(evt);
 
-    if(isCurrentIn){
-        isCurrentIn = false;
-        removeHintLocation();
-    }
-});
 
 
 
@@ -122,8 +173,3 @@ elements.pageHeader.addEventListener("dragleave", evt => {
 // 1. create file-container on the fly
 // 2. 內部顯示 “”
 
-// file 加進 file-container 之後
-// add__icon add className add__icon--small
-// add__text add className add__text--small
-// file-container add classNam u-margin-top--sm // 位置一時半會還沒調好
-// box__dragndrop display none // 位置一時半會還沒調好
